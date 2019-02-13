0e233474e2a867a41c574489257fd82b18fc59be
Gerard <gerar231@gmail.com>
2019-02-13 01:08:00 -0800

    Added basic OAuth flow.

--- src/basic_authentication.py
+++ src/basic_authentication.py
@@ -1,14 +1,22 @@
 # Step Zero: Setup HTTP information to connect to GitHub API
 # REQUIRES: http://docs.python-requests.org/en/master/user/install/
 import requests
+import json
 
 # Step One: Login to the user account through the "Basic Authentication" method.
 user_name = input("Enter your GitHub Username: ")
 password = input("Enter your GitHub Password: ")
-#authentication = requests.get("https://api.github.com/", {"userid":user_name, "password":password})
-#print(authentication.json)
+
 # Step Two: Create an OAuth Token using the Basic Authentication login and an API request.
-# authorization = requests.post("https://api.github.com/authorizations", {"note":"GitUp Authentication Token"}, json=None)
+# TO-DO: add in check for a token that has the GitUp fingerprint
+note = "GitUp Authentication Token v1.0"
+note_url = "https://github.com/gerar231/GitUp"
+authentication = requests.post("https://api.github.com/authorizations", json={"note":note, "note_url":note_url, "fingerprint":str("GitUp"+user_name)}, auth=(user_name, password))
+print(authentication.json())
+user_token = authentication.json()['token']
 
 
 # Step Three: Perform actions for a user through the API using their OAuth token.
+# example, get the number of user private repositories using their user token (not login)
+user_info = requests.get("https://api.github.com/user", params={"access_token":user_token})
+print(user_info.json())
