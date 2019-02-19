29689706fdfe0894097003639e8a38b3b912626a
kaushalm <kaushalm@localhost.localdomain>
2019-02-19 14:00:17 -0800

    Push branch work on GUI

---
+++ src/gui/gui.py
@@ -0,0 +1,79 @@
+import tkinter as tk
+from tkinter import filedialog, ttk
+
+proj_dir = None
+
+class GitUpApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.geometry('600x350')
+        self.title('GitUp')
+        self._frame = None
+        self.switch_frame(StartingMenu)
+
+    def switch_frame(self, frame_class):
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = frame_class(self)
+        self._frame.pack()
+
+class StartingMenu(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text = "GitUp").pack()
+        tk.Label (self, text = "v1.0.0").pack()
+        tk.Button(self, text = "Add Project",
+                command = lambda: master.switch_frame(AddProjectMenu)).pack()
+        tk.Button(self, text = "Open Project",
+                command = lambda: master.switch_frame(OpenProjectMenu)).pack()
+        tk.Button(self, text = "Remove Project",
+                command = lambda: master.switch_frame(DeleteProjectMenu)).pack()
+
+class AddProjectMenu(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Button(self, text = "Back",
+                command = lambda: master.switch_frame(StartingMenu))
+        tk.Button(self, text = "Create New Project",
+            command = self.createProject).pack()
+        tk.Button(self, text = "Add Existing Project").pack()
+        tk.Button(self, text = "Back",
+                command = lambda: master.switch_frame(StartingMenu)).pack()
+    def createProject(*args):
+        tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes =
+                (("jpeg   files","*.jpg"),("all files","*.*")))
+
+class OpenProjectMenu(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Button(self, text = "Back",
+                command = lambda: master.switch_frame(StartingMenu)).pack()
+        tk.Label(self, text = "Choose a project:").pack()
+        projs = ttk.Combobox(self, values = ("First project", "Second project")).pack()
+        tk.Button(self, text = "Open Project",
+            command = self.openProject(projs.current()))
+
+    def openProject(projName)
+ 
+class ProjectViewMenu(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Button(self, text = "View File History",
+                command = viewFileHistory)
+
+    def viewFileHistory():
+        tkFileDialog.askopenfilename(initialdir = proj_dir,title = "Select file to view history",
+                filetypes = (("jpeg   files","*.jpg"),("all files","*.*")))
+        
+
+class DeleteProjectMenu(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Button(self, text = "Back",
+                command = lambda: master.switch_frame(StartingMenu))
+        tk.Label(self, text = "Choose a project:").pack()
+        tk.ttk.Combobox(self, values = ("First project", "Second project")).pack()
+
+if __name__ == "__main__":
+    app = GitUpApp()
+    app.mainloop()
