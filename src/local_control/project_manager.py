ac38c25e0538e70645c02743a154f795c6abfbe3
Gerard <gerar231@gmail.com>
2019-02-20 22:28:42 -0800

    Added restore and delete project method stubs to project manager.

--- src/local_control/project_manager.py
+++ src/local_control/project_manager.py
@@ -39,6 +39,28 @@ class ProjectManager(object):
         # return the Repo object for this path
         NotImplementedError()
     
+    def restore_project_repo(self, path: str, repo_name: str):
+        """
+        Arguments:
+            path: the absolute path of the directory to restore the repository, error if path is invalid.
+            repo_name: the name of the repository as shown on GitHub, error if no invalid.
+
+        Restores the latest version of a project on a remote repository (associated with the user's GitHub
+        account) in a folder using the repo_name at the given path.
+        """
+        NotImplementedError()
+    
+    def delete_project_repo(self, path: str, remove_files=False):
+        """
+        Arguments:
+            path: the absolute path of a working tree directory of an existing local repository, error if no repo.
+            remove_files: if True remove the repository files on the user's GitHub acccount.
+        
+        Removes the origin remote from a local repository. Deletes the repository files on the users
+        GitHub account if remove_files is True.
+        """
+        NotImplementedError()
+    
     def find_project_repo(self, path: str) -> bool: 
         """
         Arguments:
