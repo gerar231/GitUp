191e1677a4513db08b1f85a9855a18d7c43363be
Gerard <gerar231@gmail.com>
2019-03-03 18:57:58 -0800

    Made github_control private, working on fixing remote push.

--- src/github_control/user_account.py
+++ src/github_control/user_account.py
@@ -30,6 +30,8 @@ class UserAccount(object):
         
         If no arguments provided:
             searches default path /tmp/GitUp/token.txt for token, if no token exists then throw ValueError.
+        
+        In all cases, if this user does not have a public key associated with this 
         """
         if token_file_path:
             norm_path = os.path.normpath(token_file_path)
@@ -51,62 +53,59 @@ class UserAccount(object):
         if existing_token:
             if user_name and password:
                 print(ValueError("A user token already exists, cannot login to a new user."))
-            self.github_control = Github(login_or_token=existing_token)
+            self.__github_control = Github(login_or_token=existing_token)
             try:
-                self.get_name()
+                self.__id = self.__github_control.get_user().id
             except GithubException.BadCredentialsException:
                 raise(ValueError("Invalid token provided in first line of file at {1}".format(token_file_path)))
+            # set token for this session
+            self.__token = existing_token
             return
 
         if user_name and password:
-            self.github_control = Github(login_or_token=user_name, password=password)
+            self.__github_control = Github(login_or_token=user_name, password=password)
             try:
-                self.get_name()
+                self.__id = self.__github_control.get_user().id
             except GithubException.BadCredentialsException:
                 raise(ValueError("Invalid user_name and/or password provided."))
             # get existing authorizations if for GitUp then delete, then create new auth token.
-            authorizations = self.github_control.get_user().get_authorizations()
+            authorizations = self.__github_control.get_user().get_authorizations()
             for auth in authorizations:
                 if auth.note_url == "https://github.com/gerar231/GitUp":
                     auth.delete()
             # create a new token
-            token = self.github_control.get_user().create_authorization(scopes=["user", "delete_repo", "repo"], note_url="https://github.com/gerar231/GitUp", 
+            token = self.__github_control.get_user().create_authorization(scopes=["user", "delete_repo", "repo"], note_url="https://github.com/gerar231/GitUp", 
                 note="GitUp Authorization token.").token
             # write the token at the default path
             with open(token_file_path, "w") as token_file:
                 print(token)
                 token_file.writelines(token)
+            # set token for this session
+            self.__token = token
 
     def get_name(self):
         """
         Returns the name associated with this user account.
         """
-        return self.github_control.get_user().name
+        return self.__github_control.get_user().name
     
     def get_id(self):
         """
         Returns the user (screen) id associated with this user account.
         """
-        return self.github_control.get_user().id
+        return self.__github_control.get_user().id
     
     def get_profile_url(self):
         """
         Returns the profile URL for the current user
         """
-        return self.github_control.get_user().html_url
+        return self.__github_control.get_user().html_url
 
     def get_profile_image_url(self):
         """
         Returns the profile image URL for the current user
         """
-        return self.github_control.get_user().avatar_url
-
-
-    def github_controller(self):
-        """
-        Returns the GitHub controller for this user (allows pushing & pulling from Repository)
-        """
-        return self.github_control
+        return self.__github_control.get_user().avatar_url
     
     def get_remote_repos(self):
         """
@@ -115,7 +114,7 @@ class UserAccount(object):
         representing the name's and clone urls of the current user's remote repositories.
         """
         repos = list()
-        for repo in self.github_control.get_user().get_repos():
+        for repo in self.__github_control.get_user().get_repos():
             repos.append(tuple([repo.name, repo.clone_url]))
         return repos
 
@@ -136,18 +135,22 @@ class UserAccount(object):
             local_repo.remote(name="GitUp")
             raise AssertionError("local_repo already has a GitUp remote.")
         except ValueError:
-            # create the remote repository
+            # attempt to create a new remote repository using the folder containing the repository name
             repo_name = os.path.basename(os.path.normpath(os.path.join(local_repo.common_dir, "..")))
-            existing_repos = self.github_control.get_user().get_repos()
+            existing_repos = self.__github_control.get_user().get_repos()
             remote_repo = None
+            # check if the user has an existing remote repository of this name
             for curr_repo in existing_repos:
                 if curr_repo.name == repo_name:
                     remote_repo = curr_repo
                     break
 
             if remote_repo is None:
-                remote_repo = self.github_control.get_user().create_repo(name=repo_name, description=str("Repository managed by GitUp."))
-                print(remote_repo.get_branches())
+                # create a new remote repository if none found
+                remote_repo = self.__github_control.get_user().create_repo(name=repo_name, description=str("Repository managed by GitUp."))
+                print("printing branches:")
+                for b in remote_repo.get_branches():
+                    print(b.name)
                 # remote repo creation failed
 
             # ERROR CHECK NEEDED
@@ -161,10 +164,11 @@ class UserAccount(object):
             curr_index.add(".")
 
             # commit 
-            curr_index.commit("Added all changes after creating remote repo.")
+            curr_index.commit("GitUp added all changes after creating remote repo.")
 
-            # push to the remote
-            if local_repo.git.push(name="GitUp") is None:
+            # push to the remote using https://username:token@remote_url.git
+            # perform a push using the git binary, specifying the url dynamically generated in the above format
+            if local_repo.remote(name="GitUp").push() is None:
                 raise exc.GitCommandError("Push to origin failed after remote repo created for {1}".format(os.path.join(local_repo.common_dir, "..")))
     
     def push_to_remote(self, local_repo):
