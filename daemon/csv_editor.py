b0cb5b8ec87edb0934f5f0e55ef648887ddf5474
Gerard <gerar231@gmail.com>
2019-03-05 19:53:54 -0800

    Updated CSV editor.

--- daemon/csv_editor.py
+++ daemon/csv_editor.py
@@ -1,10 +1,26 @@
 import csv
 import os
 
-@staticmethod
-def add_project(csv_file, path):
+csv_path = "/tmp/gitup/repositories.csv"
+
+def create_csv():
+    if os.path.exists("/tmp/gitup") is False:
+        os.mkdir("/tmp/gitup")
+    if os.path.exists(csv_path) is True:
+        return False
+    header = "last_pulled"
+    csv = open(csv_path, 'w+')
+    writer = csv.writer(csv)
+    writer.writerow(header)
+    csv.close()
+    return True    
+
+def add_project(path):
+    if os.path.exists(csv_path) is False:
+        if create_csv() is False:
+            print("CSV creation failed.")
     path = os.path.normpath(path)
-    csv = open(csv_file, 'r')
+    csv = open(csv_path, 'r')
     reader = csv.reader(csv)
     new_rows = []
     line = 0
@@ -15,15 +31,7 @@ def add_project(csv_file, path):
             new_rows.append(','.join(row))
     new_rows.append(path)
     csv.close()
-    csv = open(csv_file, 'w')
+    csv = open(csv_path, 'w')
     writer = csv.writer(csv)
     writer.writerows(new_rows)
-
-@staticmethod
-def create_csv(csv_file):
-    header = "last_pulled"
-    csv = open(csv_file, 'w+')
-    writer = csv.writer(csv)
-    writer.writerow(header)
-
-
+    csv.close()
