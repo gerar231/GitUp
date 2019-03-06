36b201c2ebcc6b1b66e30f0e0e3ab22a97171ffc
KamdenChew <40366046+KamdenChew@users.noreply.github.com>
2019-03-05 21:42:53 -0800

    Updated Build Dependencies

--- build.py
+++ build.py
@@ -3,6 +3,13 @@ from pybuilder.core import init, use_plugin
 use_plugin("python.core")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
+use_plugin("exec")
 
-
+name = "GitUp"
 default_task = "publish"
+
+@init
+def initialize(project):
+    project.build_depends_on('PyGitHub')
+    project.build_depends_on('GitPython')
+    project.build_depends_on('inotify')
