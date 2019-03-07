7e5438ff715d7a4e1350ad3fd014840a0ba63adb
Robert Kolmos <robertk3@cs.washington.edu>
2019-03-07 00:04:29 -0800

    make commit grouper also work by hunk headers

--- src/main/python/gui/commit_grouper.py
+++ src/main/python/gui/commit_grouper.py
@@ -18,3 +18,26 @@ def group_commits_by_time(commits):
         result[key] = sorted_commits
     return result
 
+def __get_commit_hunks(commit, hunk_delimiter="\n"):
+    message = commit.message
+    if ":" in message:
+        # remove the filename from the commit message.
+        message = message[message.find(":") + 2:]
+    return message.split(hunk_delimiter)
+
+def group_commits_by_hunk(commits):
+    result = {}
+    hunk_to_commits = {}
+    for commit in commits:
+        hunks = __get_commit_hunks(commit)
+        for hunk in hunks:
+            if hunk not in hunk_to_commits:
+                hunk_to_commits[hunk] = []
+            hunk_to_commits[hunk].append(commit)
+    for hunk in hunk_to_commits:
+        commits = hunk_to_commits[hunk]
+        commits_by_time = group_commits_by_time(commits)
+        if hunk not in result:
+            result[hunk] = []
+        result[hunk] = commits_by_time
+    return result
