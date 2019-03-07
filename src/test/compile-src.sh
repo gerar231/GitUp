88dba635227e4c650de1b993d39758ed8b148a1a
KamdenChew <40366046+KamdenChew@users.noreply.github.com>
2019-03-06 21:34:15 -0800

    Now Compiles all Python files in Python 3.6

--- src/test/compile-src.sh
+++ src/test/compile-src.sh
@@ -1,6 +1,21 @@
 #Compile the python files in /src folder
 cd src
-python -m compileall -l .
+python3.6 -m compileall -l .
 
 #Compile the python files in /src/gui
-python -m compileall -l ./gui
+python3.6 -m compileall -l ./gui
+
+#Compile the python files in /src/main/python/gui
+python3.6 -m compileall -l ./main/python/gui
+
+#Compile the python files in /src/main/python/daemon
+python3.6 -m compileall -l ./main/python/daemon
+
+#Compile the python files in /src/main/python/daemon_launcher
+python3.6 -m compileall -l ./main/python/daemon_launcher
+
+#Compile the python files in /src/main/python/github_control
+python3.6 -m compileall -l ./main/python/github_control
+
+#Compile the python files in /src/main/python/local_control
+python3.6 -m compileall -l ./main/python/local_control
